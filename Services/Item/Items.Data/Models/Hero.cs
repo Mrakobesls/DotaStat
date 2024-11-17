cc41dfe3f47bcff9ac48a310bd2b8ad6aabf82eb
Mrakobesls <remi007@yandex.by>
2024-11-17 21:55:09 +0300

    specify vmImage

--- Services/Item/Items.Data/Models/Hero.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Items.Data.Models;
-
-public record Hero
-{
-    public int Id { get; set; }
-    public required string Name { get; set; }
-}
--- Services/Item/Items.Data/Types/Hero.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Items.Data.Types;
-
-public class Hero
-{
-    public int Id { get; set; }
-    public required string Name { get; set; }
-}
